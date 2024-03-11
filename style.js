ab331d71972b16e893e9302f9f2e2d55bf25fd91
Chidiebele Aroh <chidiebelearoh@gmail.com>
2024-03-11 14:44:53 +0100

    first commit

---
+++ style.js
@@ -0,0 +1,13 @@
+style.js
+const hamburger = document.querySelector(".hamburger");
+const navlist = document.querySelector(".nav-list");
+
+hamburger.addEventListener("click", ()=>{
+  hamburger>clasList.toggle("active");
+  navlist.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-links").forEach (n => n.addEventListener)
+hamburger.classList.remove("active");
+navlist.classList.remove("active");
+}))
